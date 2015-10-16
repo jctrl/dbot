0f957882c4d7b8f6e2b2bfefe66cec7b5df7f5d1
listenrightmeow <mdyer@n9nemedia.net>
2015-10-15 18:46:22 -0700

    Force tfvars file declaration

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -3,5 +3,5 @@ cd .terraform
 wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.4_linux_amd64.zip
 unzip terraform_0.6.4_linux_amd64.zip
 aws s3 cp s3://dla-dbot/.terraform/terraform.tfvars ./
-ls -lha
-./terraform plan -out=terraform.plan build
+cat terraform.tfvars
+./terraform plan -var-file=terraform.tfvars -out=terraform.plan build
