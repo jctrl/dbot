3f05df7659db665f25a8f8805948cf07daa5996f
listenrightmeow <mdyer@n9nemedia.net>
2015-10-15 19:14:35 -0700

    asdfhbasdfljhasdf

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -3,4 +3,4 @@ cd .terraform
 wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.4_linux_amd64.zip
 unzip terraform_0.6.4_linux_amd64.zip
 aws s3 cp s3://dla-dbot/.terraform/terraform.tfvars ./build
-./terraform plan -out=terraform.plan build
+./terraform plan -var-file=build/terraform.tfvars -out=terraform.plan build
