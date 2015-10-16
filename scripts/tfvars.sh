7ba3a6641af3f48ca486827805b9a00d3881c269
listenrightmeow <mdyer@n9nemedia.net>
2015-10-15 18:34:52 -0700

    FML.

--- scripts/tfvars.sh
+++ scripts/tfvars.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 cd .terraform
+wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.4_linux_amd64.zip
+unzip terraform_0.6.4_linux_amd64.zip
 aws s3 cp s3://dla-dbot/.terraform/terraform.tfvars ./
+ls -lha
 ./terraform plan -out=terraform.plan build
