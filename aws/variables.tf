1162cda3e9474b8e212ff5096b09254a8be7c4d1
listenrightmeow <mdyer@n9nemedia.net>
2015-10-21 12:25:21 -0700

    Updating tfvars on S3. Adding atlas token var.

--- aws/variables.tf
+++ aws/variables.tf
@@ -8,6 +8,7 @@ variable "HUBOT_S3_BRAIN_ACCESS_KEY_ID" {}
 variable "HUBOT_S3_BRAIN_SECRET_ACCESS_KEY" {}
 variable "HUBOT_S3_BRAIN_BUCKET" {}
 variable "HUBOT_S3_BRAIN_FILE_PATH" {}
+variable "ATLAS_TOKEN" {}
 
 variable "region" {
 	default = "us-east-1"
