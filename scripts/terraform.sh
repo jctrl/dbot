59e33caabe7865d55d2ecc8199f49a5d59137b09
listenrightmeow <mdyer@n9nemedia.net>
2015-10-21 12:55:04 -0700

    Update out path for tf plan. Remove atlas token var

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -2,4 +2,4 @@
 wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.4_linux_amd64.zip
 unzip terraform_0.6.4_linux_amd64.zip
 aws s3 cp s3://dla-dbot/.terraform/terraform.tfvars ./aws
-./terraform plan -var-file=aws/terraform.tfvars -out=terraform.plan aws
+./terraform plan -var-file=aws/terraform.tfvars -out=.terraform/terraform.plan aws
