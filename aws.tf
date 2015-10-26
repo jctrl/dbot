87675c98c7151cc80db44cac4625215b7ea5657d
listenrightmeow <mdyer@n9nemedia.net>
2015-10-26 16:04:31 -0700

    modularizing bots into modules

--- aws.tf
+++ aws.tf
@@ -41,57 +41,6 @@ resource "aws_ecs_cluster" "dbot-ecs-cluster-us-east" {
   name = "dbot-ecs-cluster-us-east"
 }
 
-resource "aws_ecs_task_definition" "dbot-ecs-definition-us-east" {
-  family = "dbot-ecs-family-us-east"
-  container_definitions = <<EOF
-[
-  {
-    "name": "dbot-slackbot",
-    "image": "listenrightmeow/dbot:latest",
-    "cpu": 128,
-    "memory": 128,
-    "environment": [
-      {
-        "name" : "HUBOT_SLACK_TOKEN",
-        "value" : "${var.HUBOT_SLACK_TOKEN}"
-      },{
-        "name" : "HUBOT_AUTH_ADMIN",
-        "value" : "${var.HUBOT_AUTH_ADMIN}"
-      },{
-        "name" : "HUBOT_STANDUP_PREPEND",
-        "value" : "@group: "
-      },{
-        "name" : "HUBOT_YOUTUBE_API_KEY",
-        "value" : "${var.HUBOT_YOUTUBE_API_KEY}"
-      },{
-        "name" : "HUBOT_YOUTUBE_DETERMINISTIC_RESULTS",
-        "value" : "true"
-      },{
-        "name" : "HUBOT_S3_BRAIN_ACCESS_KEY_ID",
-        "value" : "${var.HUBOT_S3_BRAIN_ACCESS_KEY_ID}"
-      },{
-        "name" : "HUBOT_S3_BRAIN_SECRET_ACCESS_KEY",
-        "value" : "${var.HUBOT_S3_BRAIN_SECRET_ACCESS_KEY}"
-      },{
-        "name" : "HUBOT_S3_BRAIN_BUCKET",
-        "value" : "${var.HUBOT_S3_BRAIN_BUCKET}"
-      },{
-        "name" : "HUBOT_S3_BRAIN_FILE_PATH",
-        "value" : "${var.HUBOT_S3_BRAIN_FILE_PATH}"
-      }
-    ],
-    "privileged": true,
-    "portMappings": [
-      {
-        "containerPort": 8080,
-        "hostPort": 8080
-      }
-    ]
-  }
-]
-EOF
-}
-
 resource "aws_elb" "dbot-elastic-lb-us-east" {
   name = "dbot-elastic-lb-us-east"
   cross_zone_load_balancing = true
@@ -105,22 +54,6 @@ resource "aws_elb" "dbot-elastic-lb-us-east" {
 
   subnets = ["${aws_subnet.dbot-subnet-us-east.id}"]
   instances = ["${aws_instance.dbot-aws-ec2-us-east.id}"]
-}
-
-resource "aws_ecs_service" "dbot-ecs-service-us-east" {
-  name = "dbot-service"
-  cluster = "${aws_ecs_cluster.dbot-ecs-cluster-us-east.id}"
-  task_definition = "${aws_ecs_task_definition.dbot-ecs-definition-us-east.arn}"
-  desired_count = 1
-
-  iam_role = "${aws_iam_role.dbot-ecs-service-iam-role-us-east.arn}"
-  depends_on = ["aws_iam_role_policy.dbot-ecs-service-iam-policy-us-east"]
-
-  load_balancer {
-    elb_name = "${aws_elb.dbot-elastic-lb-us-east.name}"
-    container_name = "dbot-slackbot"
-    container_port = 8080
-  }
 }
 
 resource "aws_vpc" "dbot-vpc-us-east" {
--- modules.tf
+++ modules.tf
@@ -1,3 +1,4 @@
 module "brosephs" {
 	source = "./modules/brosephs.tf"
+	HUBOT_S3_BRAIN_FILE_PATH = "brain/brosephs/dump.json"
 }
